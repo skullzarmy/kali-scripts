a89ff689aa8f359ad2400bb8a083f316759d520a
skllzrmy.tez <joe@poundfit.com>
2023-03-23 15:02:11 -0700

    setup installs some needed vs code packs

--- setup.py
+++ setup.py
@@ -14,6 +14,13 @@ os.system("sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gp
 os.system("sudo sh -c 'echo \"deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main\" > /etc/apt/sources.list.d/vscode.list'")
 os.system("sudo apt update")
 os.system("sudo apt install code")
+os.system("code --install-extension akamud.vscode-theme-onedark")
+os.system("code --install-extension formulahendry.auto-rename-tag")
+os.system("code --install-extension ms-azuretools.vscode-docker")
+os.system("code --install-extension github.copilot")
+os.system("code --install-extension davidanson.vscode-markdownlint")
+os.system("code --install-extension ms-python.python")
+os.system("code --install-extension chakrounAnas.turbo-console-log")
 
 # Install OpenAI library
 os.system("sudo pip install openai")
