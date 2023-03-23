44deadced3efb5cd6f9f383620a8077a587b1831
skllzrmy.tez <joe@poundfit.com>
2023-03-22 21:06:23 -0700

    logging openai api test errors and adding a progress bar

--- scrape-git-for-loot/openai-key-tester.py
+++ scrape-git-for-loot/openai-key-tester.py
@@ -1,6 +1,7 @@
 import openai
 import sys
 import os
+from tqdm import tqdm
 
 # Enter your OpenAI API key here
 openai.api_key = "YOUR_API_KEY"
@@ -10,13 +11,13 @@ def test_api_key(api_key):
     openai.api_key = api_key
     try:
         openai.Completion.create(engine="davinci", prompt="Hello, world!")
-        return api_key
-    except:
-        return None
+        return api_key, None
+    except Exception as e:
+        return api_key, str(e)
 
 # Get the input file name from the command line argument
 if len(sys.argv) != 2:
-    print("Usage: python test_api_keys.py <input_file>")
+    print("Usage: python openai-key-tester.py <input_file>")
     sys.exit()
 input_file = sys.argv[1]
 
@@ -24,14 +25,19 @@ input_file = sys.argv[1]
 with open(input_file, "r") as f:
     # Create the output file name
     output_file = os.path.splitext(input_file)[0] + "_tested.txt"
+    # Create the error log file name
+    error_file = os.path.splitext(input_file)[0] + "_failed_tests.log"
     # Open the output file for writing
-    with open(output_file, "w") as out_f:
+    with open(output_file, "w") as out_f, open(error_file, "w") as error_f:
         # Loop through each line in the input file
-        for line in f:
+        for line in tqdm(f):
             # Remove any whitespace from the beginning and end of the line
             api_key = line.strip()
             # Test the API key
-            valid_key = test_api_key(api_key)
+            valid_key, error = test_api_key(api_key)
             # If the API key is valid, write it to the output file
             if valid_key:
                 out_f.write(valid_key + "\n")
+            # If there was an error, write it to the error log file
+            if error:
+                error_f.write(valid_key + "\t" + error + "\n")
